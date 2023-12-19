78e6f1fdc9282460b92a2ee61e52359462cd3398
Gaven Dcosta <gavendcosta@gmail.com>
2023-12-19 14:08:07 +0530

    cg

--- server/index.js
+++ server/index.js
@@ -16,11 +16,7 @@ const app = express();
 app.use(bodyParser.json({limit:"30mb", extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
 
-app.use(cors({
-  origin: "https://gaven-memories-social-media-mern.vercel.app",
-  methods: "*",
-  credentials: true
-}))
+app.use(cors())
 
 
 app.use('/posts', postRoutes)
