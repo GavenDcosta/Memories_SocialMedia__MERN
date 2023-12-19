004c953f4fe0cc3f1d0e9e95bfacce49897909f0
Gaven Dcosta <gavendcosta@gmail.com>
2023-12-19 19:42:16 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -16,7 +16,9 @@ const app = express();
 app.use(bodyParser.json({limit:"30mb", extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://gaven-memories-mern.netlify.app',
+}));
 
 
 app.use('/posts', postRoutes)
