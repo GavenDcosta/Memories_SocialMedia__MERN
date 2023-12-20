35ea5cbab92bf27334cf19265198afa23b4164a5
Gaven Dcosta <gavendcosta@gmail.com>
2023-12-20 07:43:22 +0530

    ch

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json({limit:"30mb", extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
 
 app.use(cors({
-  origin: 'https://gaven-memories-mern.netlify.app',
+  origin: 'https://rococo-cocada-00bc93.netlify.app',
 }));
 
 
